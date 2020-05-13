eae3ec8ec2e3078bf41c1906fe0c113a02efbeb1
Vinpapounet <Vinpapounet@Jacks-MacBook-Air.local>
2020-05-13 00:08:04 -0400

    svelte new project

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Route, Router } from "svelte-routing";
+
+  import Create from "./create/Create.svelte";
+  import Detail from "./detail/Detail.svelte";
+  import Library from "./library/Library.svelte";
+
+  export let url = "";
+</script>
+
+<style>
+  main {
+    padding: var(--spacingLarge);
+  }
+</style>
+
+<Router {url}>
+  <main>
+    <Route path="/create">
+      <Create />
+    </Route>
+    <Route path="/books/:id" let:params>
+      <Detail id={params.id} />
+    </Route>
+    <Route path="/">
+      <Library />
+    </Route>
+  </main>
+</Router>
